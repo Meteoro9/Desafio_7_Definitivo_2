c7e3417bf7b2f401217fac2b6384201bfdc3faca
Francisco MT Mazzone <tolal.mazzone@gmail.com>
2025-11-12 23:08:43 -0300

    Alive y Kill player SFX funcionales

--- Scripts/Player/fire_area.gd
+++ Scripts/Player/fire_area.gd
@@ -3,23 +3,36 @@ class_name FireBehaviour
 
 const max_flame := 100.0
 var current_flame := max_flame
+var is_alive := true # Para evitar loopear método kill()
 @export var bar : ProgressBar
 @export var fire_animation : AnimatedSprite2D
 
+@onready var alive_audio = $FlameAlive
+@onready var kill_audio = $FlameKill
+
 func take_damage(amount : float):
-	current_flame -= amount
+	if current_flame - amount <= 0:
+		kill()
+	else:
+		current_flame -= amount
 	
 	if current_flame < 0:
 		current_flame = 0
 
 func kill():
-	current_flame -= 100
-	fire_animation.visible = false
+	current_flame -= 100 # Forzamos <= 0
+	fire_animation.visible = false # "apagamos" el fuego animado
+	alive_audio.stop() # Apagamos el sonido de fuego vivo
+	kill_audio.play() # Reproducimos sonido de muerte
+	is_alive = false
 
 func _process(_delta: float) -> void:
-	if current_flame <= 0:
+	if current_flame <= 0 and is_alive: # Si murió en este frame
 		kill()
-	elif current_flame < max_flame: # para que no se recupere al morir
+	elif current_flame < max_flame and is_alive: # para que no se recupere al morir
 		current_flame += 0.5
 	
 	bar.value = current_flame
+
+func _on_flame_alive_finished() -> void:
+	alive_audio.play()
