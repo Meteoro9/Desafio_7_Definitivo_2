1e8a8711daedb148237186e4b9a451b367301090
Francisco MT Mazzone <tolal.mazzone@gmail.com>
2026-02-11 13:50:34 -0300

    Botón funcional

---
+++ Scripts/Elements/button.gd
@@ -0,0 +1,19 @@
+extends Area2D
+class_name ButtonBehaviour
+
+enum ButtonColor { YELLOW , RED }
+
+@export var color_state : ButtonColor = ButtonColor.YELLOW
+# Declarar variable exportada Puerta, o al revés en la puerta
+var is_active := false
+
+func _ready() -> void:
+	if color_state == ButtonColor.YELLOW: $Animation.play("default_yellow")
+	elif color_state == ButtonColor.RED: $Animation.play("default_red")
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is CandlePlayer and not is_active:
+		is_active = true
+		print("El jugador presionó el botón")
+		if color_state == ButtonColor.YELLOW: $Animation.play("pressed_yellow")
+		elif color_state == ButtonColor.RED: $Animation.play("pressed_red")
