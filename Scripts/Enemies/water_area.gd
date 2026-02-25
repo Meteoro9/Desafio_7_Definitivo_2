acb222f703232629100e6b70bdec054582d6df77
Francisco MT Mazzone <tolal.mazzone@gmail.com>
2026-02-25 18:16:10 -0300

    WaterAreas con SFX funcional

--- Scripts/Enemies/water_area.gd
+++ Scripts/Enemies/water_area.gd
@@ -3,15 +3,22 @@ class_name WaterArea
 
 var damage : float = 50.0
 var player_inside : CandlePlayer = null
+@onready var audio_player : AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _ready() -> void:
 	area_entered.connect(_on_body_entered)
+
+func play_audio() -> void:
+	var pitch = randf_range(0.7,1.3)
+	audio_player.pitch_scale = pitch
+	audio_player.play()
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is CandlePlayer:
 		player_inside = body
 		body.state_machine.change_to("StateInWater")
 		print("Entro el jugador al agua")
+		play_audio()
 	if body is FireBehaviour: body.kill()
 
 func _on_body_exited(body: Node2D) -> void:
