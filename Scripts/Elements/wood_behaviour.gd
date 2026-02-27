c580c092bbbf7038cc8d4e82b76a02504f7647bd
Francisco MT Mazzone <tolal.mazzone@gmail.com>
2026-02-27 19:24:40 -0300

    Sonido quemar madera integrado

--- Scripts/Elements/wood_behaviour.gd
+++ Scripts/Elements/wood_behaviour.gd
@@ -2,11 +2,22 @@ extends Area2D
 class_name WoodBehaviour
 
 var player_inside : CandlePlayer = null
+var audio_player : AudioStreamPlayer2D
+var wood_fire_audio : AudioStream = load("res://Audio/SFX/Elements/Wood-Fire-SFX.mp3")
+var audio_emmited : bool = false
+
+func _ready() -> void:
+	audio_player = AudioStreamPlayer2D.new()
+	add_child(audio_player)
+	audio_player.bus = "SFX"
+	audio_player.stream = wood_fire_audio
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area is FireBehaviour:
+		if !audio_emmited: audio_player.play()
 		player_inside = area.get_parent()
 		print("El player quemó la madera")
+		audio_emmited = true
 
 func _process(_delta: float) -> void:
 	if player_inside:
