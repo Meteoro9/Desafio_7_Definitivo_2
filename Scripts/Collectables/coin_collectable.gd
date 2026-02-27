c1c249216b40380341d55d1a0f88222b2038f0f7
Francisco MT Mazzone <tolal.mazzone@gmail.com>
2026-02-27 19:55:34 -0300

    Moneda con SFX y variaciones integradas

--- Scripts/Collectables/coin_collectable.gd
+++ Scripts/Collectables/coin_collectable.gd
@@ -4,8 +4,15 @@ class_name CoinCollectable
 @export var coin_material : CoinData.CoinMaterial
 @export var level_id : int
 var was_picked := false
+var audio_player : AudioStreamPlayer2D
+var coin_audio : AudioStream = load("res://Audio/SFX/Elements/Coin-Pick-SFX.mp3")
 
 func _ready() -> void:
+	audio_player = AudioStreamPlayer2D.new()
+	add_child(audio_player)
+	audio_player.stream = coin_audio
+	audio_player.pitch_scale = randf_range(0.95,1.05)
+	
 	match coin_material:
 		CoinData.CoinMaterial.GOLD: $AnimatedSprite2D.play("Gold")
 		CoinData.CoinMaterial.SILVER: $AnimatedSprite2D.play("Silver")
@@ -15,6 +22,7 @@ func _ready() -> void:
 
 func _collect(body: Node2D): 
 	if body is CandlePlayer and not was_picked: 
+		audio_player.play()
 		was_picked = true
 		GameData.register_coin(level_id, coin_material)
 		hide()
